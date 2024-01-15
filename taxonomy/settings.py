3f9c7f9019e22e21eb3c47f3570c17a06c3d44b2
jejevj <wijaya.angga.j@gmail.com>
2024-01-15 19:59:50 +0700

    add perubahan

--- taxonomy/settings.py
+++ taxonomy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7^+10_s%09_(d+&*_#+a+d11$n96if9b78lr62jz-^cs6du$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
