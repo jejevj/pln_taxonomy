c02b43c603d3af765f40ee6ce3c29abfa0e20105
jejevj <wijaya.angga.j@gmail.com>
2024-01-15 15:46:30 +0700

    deploy test

--- taxonomy/settings.py
+++ taxonomy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7^+10_s%09_(d+&*_#+a+d11$n96if9b78lr62jz-^cs6du$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
