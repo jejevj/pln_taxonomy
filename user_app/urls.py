cc575b08e6d28ac6ccc1a2715b2946e7ddc895ac
jejevj <wijaya.angga.j@gmail.com>
2024-01-17 01:49:29 +0700

    ui user clear

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from user_app.views import *
 
 urlpatterns = [
-    path('',depan,name='depan')
+    path('',depan,name='depan'),
+    path('profil',profil,name='profil'),
+    path('rekomendasi-bidang',rekomendasi_bidang,name='rekomendasi-bidang'),
+    path('rekomendasi-jabatan',rekomendasi_jabatan,name='rekomendasi-jabatan'),
+    path('rekomendasi-kompetensi',rekomendasi_kompetensi,name='rekomendasi-kompetensi'),
+    path('detail-kompetensi',detail_kompetensi,name='detail-kompetensi'),
+    path('list-taxonomy',list_taxonomy,name='list-taxonomy'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -2,3 +2,22 @@ from django.shortcuts import render
 # Create your views here.
 def depan(request):
     return render(request,'index_user.html')
+
+def profil(request):
+    return render(request,'user_profile.html')
+
+def rekomendasi_bidang(request):
+    return render(request,'rekomendasi_bidang.html')
+
+def rekomendasi_jabatan(request):
+    return render(request,'rekomendasi_jabatan.html')
+
+def rekomendasi_kompetensi(request):
+    return render(request,'kompetensi.html')
+
+def detail_kompetensi(request):
+    return render(request,'detail_kompetensi.html')
+
+def list_taxonomy(request):
+    return render(request,'list_taxonomy.html')
+    
