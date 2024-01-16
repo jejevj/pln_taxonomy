de01969609b0f18cc89216af34881f60fec33f2c
jejevj <wijaya.angga.j@gmail.com>
2024-01-17 02:34:53 +0700

    admin 25%

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Bidang(models.Model):
+    id_bidang = models.AutoField(primary_key=True)
+    nama_bidang = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nama_bidang
